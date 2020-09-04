f257e9e5a06e0da986630d3ed114a70bcdff44a9
bobo mallick <mallickbobo@gmail.com>
2020-09-05 01:09:27 +0530

    material-ui install is done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import { Button } from '@material-ui/core';
 
 function App() {
   return (
     <div>
       <h1>Social Buddy</h1>
+      <Button color="primary">Hello World</Button>;
     
     </div>
   );
